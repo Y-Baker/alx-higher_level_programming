8c9f94a574848a1b07c15b104ab8dff77ae03072
Youssef ( <yuossefbakier@gmail.com>
2023-06-28 10:41:46 -0700

    Add file 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,20 +2,21 @@
 
 int is_palindrome(listint_t **head)
 {
-    if (!*head || !(**head).next)
-        return (1);
-    listint_t *first;
-    listint_t *last;
-    int n, num;
+	if (!*head || !(**head).next)
+		return (1);
+	listint_t *first;
+	int n;
 
-    first = *head, n = first->n;
-    while (first->next->next)
-    {
-        first = first->next;
-    }
-    if ((first->next)->n != n)
-        return (0);
-    first->next = NULL;
-    first = (*head)->next;
-    is_palindrome(&first);
+	first = *head, n = first->n;
+	while (first->next->next)
+	{
+		first = first->next;
+	}
+	if ((first->next)->n != n)
+		return (0);
+	first->next = NULL;
+	first = (*head)->next;
+	is_palindrome(&first);
+	add_nodeint_end(head, n);
+	return (1);
 }
