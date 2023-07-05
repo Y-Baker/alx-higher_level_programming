1e1cd89a56dac65961de83e4700a71806d6fcd84
Youssef ( <yuossefbakier@gmail.com>
2023-07-05 04:58:24 -0700

    Add File 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 
+sorce = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
 def roman_to_int(roman_string):
-    sorce = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if roman_string == 'None' or not roman_string:
+        return 0
     value = 0
+    idx = 0
     for letter in roman_string:
-        if (letter == 'I'):
-            if (roman_string[-1] != 'I'):
-                value -= 1
-            else:
-                value += 1
+        if (check_sub(roman_string[idx:], sorce[letter])):
+            value -= sorce[letter]
         else:
             value += sorce[letter]
+        idx += 1
     return value
+
+def check_sub(string, value):
+    for x in string:
+        if sorce[x] > value:
+            return True
+    return False
