da247a65a038376ed0a57adb33316979e66c517c
Youssef ( <yuossefbakier@gmail.com>
2023-07-14 08:54:22 -0700

    Add Files 2,102

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-from __future__ import print_function
-import sys
 
+import sys
 
 def safe_function(fct, *args):
     try:
-        res = fct(*args)
-    except Exception as e:
-        print("Exception: {}".format(e), file=sys.stderr)
-        return None
-    else:
-        return res
+        n = fct(*args)
+    except Exception as x:
+        print("Exception: {}".format(x), file=sys.stderr)
+        n = None
+    return (n)
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    """Returns the division of a by b."""
     try:
-        div = a / b
-    except (TypeError, ZeroDivisionError):
-        div = None
+        re = a / b
+    except (ZeroDivisionError, TypeError):
+        re = None
     finally:
-        print("Inside result: {}".format(div))
-    return (div)
+        print("Inside result: {}".format(re))
+        return (re)
