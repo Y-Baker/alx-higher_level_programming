2718e2483170797a34cce0a8d65dbdacc8fdd9bb
Youssef ( <yuossefbakier@gmail.com>
2023-07-26 06:08:44 -0700

    Fix File 101

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -9,4 +9,4 @@ def lazy_matrix_mul(m_a, m_b):
     Return mul of two matrix m_a and m_b
     Return the mul matrix by numpy library
     """
-    return (numpy.matmuL(m_a, m_b))
+    return (numpy.matmul(m_a, m_b))
