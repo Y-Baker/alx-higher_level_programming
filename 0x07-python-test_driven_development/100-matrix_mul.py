5160e5158c7cdbfccb3bc94f12425380e3c25dfd
Youssef ( <yuossefbakier@gmail.com>
2023-07-26 05:52:57 -0700

    Add File 100

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,67 +1,88 @@
 #!/usr/bin/python3
-"""Defines a matrix multiplication function."""
+
+"""The Module that have matrix mul fun"""
 
 
 def matrix_mul(m_a, m_b):
-    """Multiply two matrices.
-
-    Args:
-        m_a (list of lists of ints/floats): The first matrix.
-        m_b (list of lists of ints/floats): The second matrix.
-    Raises:
-        TypeError: If either m_a or m_b is not a list of lists of ints/floats.
-        TypeError: If either m_a or m_b is empty.
-        TypeError: If either m_a or m_b has different-sized rows.
-        ValueError: If m_a and m_b cannot be multiplied.
-    Returns:
-        A new matrix representing the multiplication of m_a by m_b.
+    """the mul function
+    m_a: list of int or float
+    m_b: list of int or float
     """
-
-    if m_a == [] or m_a == [[]]:
-        raise ValueError("m_a can't be empty")
-    if m_b == [] or m_b == [[]]:
-        raise ValueError("m_b can't be empty")
-
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
-
-    if not all(isinstance(row, list) for row in m_a):
+    if not all(isinstance(element, list)for element in m_a):
         raise TypeError("m_a must be a list of lists")
-    if not all(isinstance(row, list) for row in m_b):
+    if not all(isinstance(element, list) for element in m_b):
         raise TypeError("m_b must be a list of lists")
-
+    if m_a == [] or m_a == [[]]:
+        raise ValueError("m_a can't be empty")
+    if m_b == [] or m_b == [[]]:
+        raise ValueError("m_b can't be empty")
     if not all((isinstance(ele, int) or isinstance(ele, float))
-               for ele in [num for row in m_a for num in row]):
+               for row in m_a for ele in row):
         raise TypeError("m_a should contain only integers or floats")
     if not all((isinstance(ele, int) or isinstance(ele, float))
-               for ele in [num for row in m_b for num in row]):
+               for row in m_b for ele in row):
         raise TypeError("m_b should contain only integers or floats")
 
-    if not all(len(row) == len(m_a[0]) for row in m_a):
-        raise TypeError("each row of m_a must should be of the same size")
-    if not all(len(row) == len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_b must should be of the same size")
+    size = len(m_a[0])
+    if not all(len(row) == size for row in m_a):
+        raise TypeError("each row of m_a must be of the same size")
+    size = len(m_b[0])
+    if not all(len(row) == size for row in m_b):
+        raise TypeError("each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
-    inverted_b = []
-    for r in range(len(m_b[0])):
-        new_row = []
-        for c in range(len(m_b)):
-            new_row.append(m_b[c][r])
-        inverted_b.append(new_row)
+    mul = mul_matrix(m_a, m_b)
+    return mul
 
-    new_matrix = []
-    for row in m_a:
-        new_row = []
-        for col in inverted_b:
-            prod = 0
-            for i in range(len(inverted_b[0])):
-                prod += row[i] * col[i]
-            new_row.append(prod)
-        new_matrix.append(new_row)
 
+def reverse_matrix(matrix):
+    """
+    Reverse Matrix
+    matrix: list of list of int or float
+    return: the reverse matrix
+    """
+    if not matrix:
+        return None
+    cols = len(matrix)
+    rows = len(matrix[0])
+
+    new_matrix = [[0 for row in range(cols)] for col in range(rows)]
+    for row in range(rows):
+        for col in range(cols):
+            new_matrix[row][col] = matrix[col][row]
     return new_matrix
+
+
+def mul_two_rows(l_a, l_b):
+    """multiple to rows for mulitple two matrix"""
+    re = 0
+    for n in range(len(l_a)):
+        re += l_a[n] * l_b[n]
+    return re
+
+
+def mul_matrix(m_a, m_b):
+    """
+    Multiplicate two matrix
+    m_a: list of list of int or float
+    m_b: list of list of int or float
+    Return: the mul matrix
+    """
+    if not m_a or not m_b:
+        return None
+    rows = len(m_a)
+    cols = len(m_b[0])
+    m_b = reverse_matrix(m_b)
+
+    mul = [[0 for row in range(cols)] for col in range(rows)]
+    for row in range(rows):
+        for col in range(cols):
+            mul[row][col] = mul_two_rows(m_a[row], m_b[col])
+
+    return mul
