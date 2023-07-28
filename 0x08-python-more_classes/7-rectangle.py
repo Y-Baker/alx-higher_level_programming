6aa78b879f304927fb8d426c85aaee39b1c6622e
Youssef ( <yuossefbakier@gmail.com>
2023-07-28 11:44:36 -0700

    Add File 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,36 +1,34 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+"""New Module"""
 
 
 class Rectangle:
-    """Represent a rectangle.
-
-    Attributes:
-        number_of_instances (int): The number of Rectangle instances.
-        print_symbol (any): The symbol used for string representation.
-    """
-
+    """Represent Opject Rectangle"""
     number_of_instances = 0
-    print_symbol = "#"
+    print_symbol = '#'
+
+    @classmethod
+    def number():
+        pass
 
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
-
-        Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
         """
-        type(self).number_of_instances += 1
+        Create New Rectangle
+        width: int
+        height: int
+        """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
-        """Get/set the width of the Rectangle."""
+        """Width Property [Getter]"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Width [Setter]"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -39,11 +37,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get/set the height of the Rectangle."""
+        """Height Property [Getter]"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Height [Setter]"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -51,37 +50,35 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Return the area of the Rectangle."""
-        return (self.__width * self.__height)
+        """The Area Method"""
+        area = self.__width * self.__height
+        return area
 
     def perimeter(self):
-        """Return the perimeter of the Rectangle."""
-        if self.__width == 0 or self.__height == 0:
-            return (0)
-        return ((self.__width * 2) + (self.__height * 2))
+        """The Perimeter Method"""
+        if self.__width * self.__height == 0:
+            return 0
+        perimeter = (self.__width + self.__height) * 2
+        return perimeter
 
     def __str__(self):
-        """Return the printable representation of the Rectangle.
-
-        Represents the rectangle with the # character.
-        """
-        if self.__width == 0 or self.__height == 0:
-            return ("")
-
-        rect = []
-        for i in range(self.__height):
-            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
-            if i != self.__height - 1:
-                rect.append("\n")
-        return ("".join(rect))
+        """the special method that called when you call print fun"""
+        if self.__width * self.__height == 0:
+            return ""
+        shape = []
+        for row in range(self.__height):
+            for col in range(self.__width):
+                shape.append(str(Rectangle.print_symbol))
+            shape.append('\n')
+        shape.pop()
+        return (''.join(shape))
 
     def __repr__(self):
-        """Return the string representation of the Rectangle."""
-        rect = "Rectangle(" + str(self.__width)
-        rect += ", " + str(self.__height) + ")"
-        return (rect)
+        """The Official represent of the object"""
+        str = f"{type(self).__name__}({self.__width}, {self.__height})"
+        return str
 
     def __del__(self):
-        """Print a message for every deletion of a Rectangle."""
-        type(self).number_of_instances -= 1
+        """Called when the object is deleted"""
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
