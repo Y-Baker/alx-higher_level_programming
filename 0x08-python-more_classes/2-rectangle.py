826b99267078db1de3fa18b5b6c6493788fcccaa
Youssef ( <yuossefbakier@gmail.com>
2023-07-28 08:30:07 -0700

    Add File 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+"""New Module"""
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """Represent Opject Rectangle"""
 
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
-
-        Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
+        """
+        Create New Rectangle
+        width: int
+        height: int
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Get/set the width of the Rectangle."""
+        """Width Property [Getter]"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Width [Setter]"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -30,11 +30,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get/set the height of the Rectangle."""
+        """Height Property [Getter]"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Height [Setter]"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -42,11 +43,13 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Return the area of the Rectangle."""
-        return (self.__width * self.__height)
+        """The Area Method"""
+        area = self.width * self.height
+        return area
 
     def perimeter(self):
-        """Return the perimeter of the Rectangle."""
-        if self.__width == 0 or self.__height == 0:
-            return (0)
-        return ((self.__width * 2) + (self.__height * 2))
+        """The Perimeter Method"""
+        if self.width * self.height != 0:
+            return 0
+        perimeter = (self.width + self.height) * 2
+        return perimeter
