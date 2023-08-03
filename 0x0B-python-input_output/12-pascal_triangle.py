aa36aefd57e08d721fb4174e4f13b2772de242d1
Youssef ( <yuossefbakier@gmail.com>
2023-08-03 12:43:32 -0700

    Add File 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-"""Defines a Pascal's Triangle function."""
+
+"""New Module for representing the Pascal's triangle of n"""
 
 
 def pascal_triangle(n):
-    """Represent Pascal's Triangle of size n.
+    """Return list of list for pascal triangle"""
 
-    Returns a list of lists of integers representing the triangle.
-    """
     if n <= 0:
         return []
 
-    triangles = [[1]]
-    while len(triangles) != n:
-        tri = triangles[-1]
-        tmp = [1]
-        for i in range(len(tri) - 1):
-            tmp.append(tri[i] + tri[i + 1])
-        tmp.append(1)
-        triangles.append(tmp)
-    return triangles
+    re_list = [[] for _ in range(n)]
+    for row in range(n):
+        for col in range(row + 1):
+            if col == 0 or col == row:
+                re_list[row].append(1)
+            else:
+                if col > row / 2:
+                    num = re_list[row][row - col]
+                    re_list[row].append(num)
+                else:
+                    num = re_list[row - 1][col - 1] + re_list[row - 1][col]
+                    re_list[row].append(num)
+    return re_list
