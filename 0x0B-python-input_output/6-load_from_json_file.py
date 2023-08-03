63f220a919fd3bae071fe487f7e1e71de911fbcb
Youssef ( <yuossefbakier@gmail.com>
2023-08-03 08:31:03 -0700

    Add File 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""Defines a JSON file-reading function."""
+
+"""New Module for creates an Object from a JSON file"""
 import json
 
 
 def load_from_json_file(filename):
-    """Create a Python object from a JSON file."""
+    """convert from str JSON to PY object"""
+
     with open(filename) as f:
         return json.load(f)
