66bbc791b6d7c390a89c4fa0f37db06cb13d0d85
Youssef ( <yuossefbakier@gmail.com>
2023-08-03 12:09:49 -0700

    Add File 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,41 +1,31 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+
+"""New Module for Student Class"""
 
 
 class Student:
-    """Represent a student."""
+    """New Class Called Student"""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+        """Initialize Emety Student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
-
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-
-        Args:
-            attrs (list): (Optional) The attributes to represent.
-        """
+        """Return Dictinart representation"""
         if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+            all(type(element) == str for element in attrs)):
+            new_dict = dict()
+            for attr in sorted(attrs):
+                if attr in self.__dict__:
+                    new_dict[attr] = self.__dict__[attr]
+            return new_dict
+
         return self.__dict__
 
     def reload_from_json(self, json):
-        """Replace all attributes of the Student.
-
-        Args:
-            json (dict): The key/value pairs to replace attributes with.
-        """
-        for k, v in json.items():
-            setattr(self, k, v)
+        """that replaces all attributes of the Student instance"""
+        for key in json:
+            if key in self.__dict__:
+                self.__dict__[key] = json[key]
