6f380e88b86c6c20e67798bf239f3917cab944c7
Youssef ( <yuossefbakier@gmail.com>
2023-08-03 07:47:24 -0700

    Add File 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Defines a text file-reading function."""
+
+"""New Module For Write a function that reads a text file
+(UTF8) and prints it to stdout:"""
 
 
 def read_file(filename=""):
-    """Print the contents of a UTF8 text file to stdout."""
+    """New Function For read File and Print data"""
+
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
