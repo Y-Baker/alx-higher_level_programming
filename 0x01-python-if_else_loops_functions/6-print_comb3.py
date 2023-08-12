2acf75abae84cc578401d9a948599538a9365d92
Youssef ( <yuossefbakier@gmail.com>
2023-08-12 16:26:48 -0700

    Fix Code Style Errors

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ i = 0
 while i < 9:
     j = i + 1
     while j < 10:
-        if(i == 8 and j == 9):
+        if (i == 8 and j == 9):
             print("{:d}{:d}".format(i, j))
         else:
             print("{:d}{:d}".format(i, j), end=", ")
