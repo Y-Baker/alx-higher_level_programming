dcf54e1e57f0907d43efce26243342e61c3d1347
Youssef ( <yuossefbakier@gmail.com>
2023-08-18 11:48:12 -0700

    Add File 15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,9 @@
--- lists all Comedy shows in the database hbtn_0d_tvshows
--- lists all rows of a database corresponding to a column value
-SELECT title
+-- lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT tv_shows.title
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = 'Comedy'
-GROUP BY title
-ORDER BY title ASC;
+ORDER BY tv_shows.title ASC;
