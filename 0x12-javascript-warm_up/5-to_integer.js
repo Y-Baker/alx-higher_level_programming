4d052005f2581adff109758fa78932191b4d80c6
Yousef <yuossefbakier@gmail.com>
2023-09-12 14:49:42 +0300

    add File 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+
+if (isNaN(process.argv[2])) {
   console.log('Not a number');
 } else {
-  console.log('My number:', parseInt(process.argv[2]));
+  console.log(parseInt(process.argv[2]));
 }
