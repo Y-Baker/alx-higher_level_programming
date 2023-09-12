7c943113bc27b806ce588c2e24d5e9ce03b5ca7f
Yousef <yuossefbakier@gmail.com>
2023-09-12 22:59:39 +0300

    Add File 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
+
 if (process.argv.length <= 3) {
   console.log('0');
 } else {
-  const arr = process.argv.slice(2).map(Number);
-  const second = arr.sort(function (a, b) { return b - a; })[1];
-  console.log(second);
+  let array = process.argv.slice(2);
+  array = array.map(Number);
+  array.sort((a, b) => b - a);
+  console.log(array[1]);
 }
