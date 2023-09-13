1ce1810d0ac9a46b30a9f3da37a231a7d7b5411a
Yousef <yuossefbakier@gmail.com>
2023-09-13 18:00:29 +0300

    Add File 4

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,15 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if ((w > 0) && (h > 0)) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
   print () {
-    for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += 'X';
-      }
-      console.log(s);
+    for (let h = 0; h < this.height; h++) {
+      console.log('X'.repeat(this.width));
     }
   }
 }
