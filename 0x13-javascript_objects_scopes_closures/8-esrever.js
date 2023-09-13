d849e56cc6e3f20888b34ae5139a9a569f6f716c
Yousef <yuossefbakier@gmail.com>
2023-09-13 18:51:22 +0300

    Add File 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  let newList;
+  let newList = Array();
 
-  for (let i = list.lenght - 1; i >= 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
     newList.push(list[i]);
   }
 
