4284e79153186d67bd309cff3893e15a8a8a9cf9
Yousef <yuossefbakier@gmail.com>
2023-10-05 03:22:18 +0300

    Add File 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""A script that
-- takes in a URL
-- sends a request to the URL
-- displays the body of the response.
+
 """
-import sys
+takes in a URL, sends a request to the URL and
+displays the body of the response or the error code state
+"""
+
+
 import requests
+import sys
 
-
-if __name__ == "__main__":
-    url = sys.argv[1]
-
-    r = requests.get(url)
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
+if __name__ == '__main__':
+    res = requests.get(sys.argv[1])
+    if res.status_code >= 400:
+        print('Error code: {}'.format(res.status_code))
     else:
-        print(r.text)
+        print(res.text)
