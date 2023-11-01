e1e000b4a0a730e305c939811817cbec5a4b62d7
Yousef <yuossefbakier@gmail.com>
2023-11-01 18:09:43 +0200

    Add File 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
-const request = require('request');
-request.get(process.argv[2]).on('response', function (response) {
-  console.log(`code: ${response.statusCode}`);
-});
+
+const request = require('request')
+
+request(process.argv[2], (err, response) => {
+  if (err) {
+    console.log(err)
+  }
+  else {
+    console.log('code: ' + response.statusCode)
+  }
+})
