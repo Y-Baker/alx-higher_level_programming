1ab5f31a315ee04e30f356abe1c4cc46bc9c386f
Yousef <yuossefbakier@gmail.com>
2023-11-01 22:10:13 +0200

    FINISH ✨

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -7,14 +7,14 @@ const apiUrl = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(apiUrl, (err, res, body) => {
   if (!err && res.statusCode === 200) {
     body = JSON.parse(body);
-    characters = body.characters;
+    const characters = body.characters;
     for (const character of characters) {
       request(character, (err, res, characterBody) => {
         if (!err && res.statusCode === 200) {
-          data = JSON.parse(characterBody);
+          const data = JSON.parse(characterBody);
           console.log(data.name);
         }
-      })
+      });
     }
   }
 });
