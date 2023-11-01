962e6b315d95ef0f2e08b4d26579503b19d6d944
Yousef <yuossefbakier@gmail.com>
2023-11-01 20:00:52 +0200

    Fix Code Style

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,14 +2,14 @@
 
 const request = require('request');
 
-apiUrl = process.argv[2];
+const apiUrl = process.argv[2];
 request(apiUrl, (err, res, body) => {
   console.log('here');
   if (!err && res.statusCode === 200) {
     const dict = {};
     body = JSON.parse(body);
     for (const task of body) {
-      if (task.completed == true) {
+      if (task.completed === true) {
         if (dict[task.userId]) dict[task.userId]++;
         else dict[task.userId] = 1;
       }
