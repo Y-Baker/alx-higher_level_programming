d67ec2e902a607b99bb90d85f41826d1f0f3ae50
Yousef <yuossefbakier@gmail.com>
2023-11-01 18:23:18 +0200

    Add File 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
 
 const request = require('request');
-const episodeNum = process.argv[2];
-const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
-request(API_URL + episodeNum, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const responseJSON = JSON.parse(body);
-    console.log(responseJSON.title);
-  } else {
-    console.log('Error code: ' + response.statusCode);
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request(apiUrl, (err, res, body) => {
+  if (!err && res.statusCode === 200) {
+    console.log(JSON.parse(body).title);
   }
 });
