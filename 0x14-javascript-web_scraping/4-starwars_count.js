658c8f34123c30682b5a8ff69f7555a6701eb424
Yousef <yuossefbakier@gmail.com>
2023-11-01 19:22:44 +0200

    Add File 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,15 +4,17 @@ const request = require('request');
 
 const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
 const character = { id: 18, name: 'Wedge Antilles' };
-const characterApi = `https://swapi-api.alx-tools.com/api/people/${character.id}/`;
 
 request(apiUrl, (err, res, body) => {
   if (!err && res.statusCode === 200) {
     body = JSON.parse(body);
     let count = 0;
     for (const film of body.results) {
-      if (film.characters.includes(characterApi)) {
-        count += 1;
+      const characters = film.characters;
+      for (let i = 0; i < characters.length; i++) {
+        if (characters[i].endsWith(character.id + '/')) {
+          count++;
+        }
       }
     }
     console.log(count);
